cca8f1b6dd187970dbd786e5c574b8dee46b4d65
Jose Miguel Cejas Martin <jcejmar148@g.educaand.es>
2024-05-08 11:25:16 +0200

    Agregado practica-146.php al repositorio

---
+++ practica-146.php
@@ -0,0 +1,48 @@
+<?php
+/**
+*@author Jose Miguel Cejas Martin
+*@version 1
+*/
+
+//Comentario de una línea para devs
+
+/*
+Comentario
+de varias
+líneas
+para devs
+*/
+
+echo "Añadida esta línea de código";
+
+/**
+*Suma 2 números
+*Esta función recibe 2 números y devuelve la suma de estos
+*@param int $param1 Primer número a sumar
+*@param int $param2 Segundo número a sumar
+*@return int Resultado de la suma
+*/
+
+function suma($param1, $param2){
+
+$resultado = $param1+$param2;
+
+return $resultado;
+
+}
+
+/**
+*Resta 2 números
+*Esta función recibe 2 números y devuelve la resta de estos
+*@param int $param1 Primer número a restar
+*@param int $param2 Segundo número a restar
+*@return int Resultado de la resta
+*/
+
+function resta($param1, $param2){
+
+$resultado= $param1-$param2;
+
+return $resultado;
+
+}
